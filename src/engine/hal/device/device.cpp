78c35609f90a1d2babd10959aa0bb9dc12be74b3
huanghuiqing <huiqing.huang-e0075@aqara.com>
2026-01-13 11:07:21 +0800

    build: add display resolution configuration to CMake and update device initialization

--- src/engine/hal/device/device.cpp
+++ src/engine/hal/device/device.cpp
@@ -1,6 +1,6 @@
 #include "./device.hpp"
 
-#define DISP_BUF_SIZE (1024 * 600)
+#define DISP_BUF_SIZE (DISP_HOR_RES * DISP_VER_RES)
 
 void hal_init () {
     /*LittlevGL init*/
@@ -21,8 +21,8 @@ void hal_init () {
     lv_disp_drv_init(&disp_drv);
     disp_drv.draw_buf   = &disp_buf;
     disp_drv.flush_cb   = fbdev_flush;
-    disp_drv.hor_res    = 1024;
-    disp_drv.ver_res    = 600;
+    disp_drv.hor_res    = DISP_HOR_RES;
+    disp_drv.ver_res    = DISP_VER_RES;
     lv_disp_drv_register(&disp_drv);
 
 	/* Linux input device init */
@@ -35,6 +35,7 @@ void hal_init () {
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = evdev_read;   //lv_gesture_dir_t lv_indev_get_gesture_dir(const lv_indev_t * indev)
     lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
+    (void)my_indev;
 };
 
 uint32_t custom_tick_get(void)
